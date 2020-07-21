cf06033da8e202a175d47230478f66092d91708d
yyj-star <jackyyjyang@gmail.com>
2020-07-21 18:12:32 -0400

    artworks collection skeleton

---
+++ data/artwork.js
@@ -0,0 +1,31 @@
+
+let exportFunctions ={
+
+    async createArtwork(){
+    
+    },
+    
+    async updateArtwork(){
+    
+    },
+    
+    async deleteArtwork(id){
+    
+    },
+    
+    async getArtworkById(id){
+    
+        //return artwork
+    },
+    
+    async getArtWorksByUserId(userId){
+        
+        //return array of artworks
+    },
+    
+    async getAllArtWorks(){
+    
+        //return array of artworks
+    },
+    };
+    module.exports = exportFunctions;

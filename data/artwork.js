f412dd17c4c06c23b18aa6b04bc56b5dcfb686a7
yyj-star <jackyyjyang@gmail.com>
2020-07-26 19:53:21 -0400

    mongoose 1st draft

--- data/artwork.js
+++ data/artwork.js
@@ -3,127 +3,165 @@ const artworks = mongoCollections.artworks;
 const users = require('./users');
 const uuid = require('uuid/v4');
 
+const models = require('../models');
+const validators = require('./validators');
+const users = require('./users');
+
 
 let exportFunctions ={
 
     async getArtworkById(id){
-        if (!id) throw 'You must provide an id to search for';
-        const artworksCollection = await artworks();
-        const artwork = await artworksCollection.findOne({_id:id});
-        if (artwork === null) throw 'No artwork with that id';
-
-        return artwork;
+        if (!validators.isNonEmptyString(id)) throw 'Id is not valid';
+        const artWork = await models.Artwork.findById(id).exec();
+        if (!artWork) throw `There is no artWork with that ID: ${id}`;
+        return artWork;
     },
 
      async getArtWorksByUserId(userId){
-        const artworksCollection = await artworks();
-
-        const artworksByUserList = await artworksCollection.findOne({userId: userId}).toArray();
-    
-        return artworksByUserList;
+        if (!validators.isValidUserId(userId)) throw 'userId is not valid';
+        return await models.Artwork.find({ userId: userId }).exec();
     },
     
     async getAllArtWorks(){
-        const artworksCollection = await artworks();
 
-        const artworksList = await artworksCollection.find({}).toArray();
-    
-        return artworksList;
+        return await models.Artwork.find({}).exec();
     },
 
-    async createArtwork(title, category, userId, description, createDate){
-    if (!title) throw 'You must provide a title';
-    if (!category) throw 'You must provide a category';
-    if (!userId) throw 'You must provide a userId';
-    if (!description) throw 'You must provide a description';
-    if (!postDate) throw 'You must provide a postDate';
-    if (!createDate) throw 'You must provide a createDate';
+    async createArtwork(title, description, category, createDate, userId, pictures) {
+        if (!title) throw 'You must provide a title';
+        if (!category) throw 'You must provide a category';
+        if (!userId) throw 'You must provide a userId';
+        if (!description) throw 'You must provide a description';
+        if (!createDate) throw 'You must provide a createDate';
 
-    const artworksCollection = await artworks();
     const userObj = await users.getUserById(userId); 
     const userName = [userObj.firstName, userObj.lastName];
+    const newArtwork = new models.Artwork({
+      title,
+      description,
+      category,
+      createDate,
+      userName,
+      userId,
+      pictures,
+      numberOfViews,
+      lastView
+    }); // Other attributes will set to the default value
 
-    const newArtwork = {
-        _id: uuid(),  
-        title: title,
-        category: category, 
-        name: userName,
-        userId: userId,
-        description: description,
-        postDate: Date.now(),   
-        createDate: createDate,
-        numberOfViews: 0,       
-        lastView: postDate,     //to be changed later with functions i.e. getLastViewDate()
-        pictures:[ 
-            //content: getPicturesByArtWorkId(_id); 
-        ],
-        comments:[
-            //content: getCommentsByArtworkId(_id)
-        ]
-    };
-    const insertInfo = await artworksCollection.insertOne(newArtwork);
-    if (insertInfo.insertedCount === 0) throw 'Could not add artwork';
-
-    const newArtwork = await this.getArtworkById(insertInfo.insertedId);
-
-    return newArtwork;
-
-    },
-    
-    async updateArtwork(id, updatedArtwork){
-        const artworksCollection = await artworks();
-        const userName = await users.getUserById(userId); 
-
-        const updatedArtworksData = {};
-
-    if (updatedArtwork.title) {
-        updatedArtworksData.title = updatedArtwork.title;
-    }
-    if (updatedArtwork.category) {
-        updatedArtworksData.category = updatedArtwork.category;
-    }
-    if (updatedArtwork.description) {
-        updatedArtworksData.description = updatedArtwork.description;
-    }
-    if (updatedArtwork.createDate) {
-        updatedArtworksData.createDate = updatedArtwork.createDate;
-    }
-    if (updatedArtwork.numberOfViews) {
-        updatedArtworksData.numberOfViews = updatedArtwork.numberOfViews;
-    }
-    if (updatedArtwork.lastView) {
-        updatedArtworksData.lastView = updatedArtwork.lastView;
-    }
-    
-    
-
-    await artworksCollection.updateOne({ _id: id }, { $set: updatedArtworksData });
-
-    return await this.getArtworkById(id);
+    const createdArtwork = await saveSafely(newArtwork);
+    return createdArtwork;
   },
-    
-    async deleteArtwork(id){
-        const artworksCollection = await artworks();
-        let artwork = null;
-        try {
-          artwork = await this.getArtworkById(id);
-        } catch (e) {
-          console.log(e);
-          return;
-        }
-        const deletionInfo = await artworksCollection.removeOne({ _id: id });
-        if (deletionInfo.deletedCount === 0) {
-          throw `Could not delete artwork with id of ${id}`;
-        }
-        //await users.removeArtworkFromUser(artwork_id, user_id);
-        
-        // removeArtworkFromUser
-        // addArtworkToUser
+  async updateArtwork(id, artwork) {
+    if (!validators.isNonEmptyString(id)) throw 'Please provide an artwork ID';
+    if (!artwork) throw 'Please provide an artwork to update with';
 
-        return true;
+    const oldArtwork = await models.Artwork.findById(id).exec(); // Don't forgot to call "exec()" for any query
+    if (!oldArtwork) throw `There is no artwork with the given ID: ${id}`;
+
+    if (artwork.title) {
+       oldArtwork.title = artwork.title;
+    }
+    if (artwork.category) {
+        oldArtwork.category = artwork.category;
+      }
+    if (artwork.description) {
+        oldArtwork.description = artwork.description;
+    }
+    if (artwork.createDate) {
+        oldArtwork.createDate = artwork.createDate;
+    }
+    if (artwork.numberOfViews) {
+        oldArtwork.numberOfViews = artwork.numberOfViews;
+    }
+    if (artwork.lastView) {
+        oldArtwork.lastView = artwork.lastView;
+    }
+    return await saveSafely(oldArtwork); // Save the the changes
+  },
+
+    async deleteArtwork(id){
+        if (!validators.isNonEmptyString(id))
+        throw 'Please provide an id to delete';
+        const deletedArtwork = await models.Artwork.findByIdAndDelete(id).exec();
+        return deletedArtwork;
     },
+    async getArtworksByKeyword(keyword) {
+        if (!validators.isNonEmptyString(keyword)) throw 'Please provide a keyword to search by';
+        return await models.Artwork.find({
+          $or: [{ title: new RegExp(keyword, 'i') }, { description: new RegExp(keyword, 'i') }],
+        }).exec();
+      },
     
+      async getArtworksByCategory(category) {
+        if (!validators.isNonEmptyString(category)) throw 'Please provide a category to search by';
+        return await models.Artwork.find({ category: new RegExp(category, 'i') }).exec();
+      },
+    
+      async getArtworksByUsername(username) {
+        if (!validators.isNonEmptyString(username)) throw 'Please provide a username to search by';
+    
+        return await models.Artwork.find({ username: new RegExp(username, 'i') }).exec();
+      },
+    
+      async getArtworksByViews(skips = 0, count = 10) {
+        return await genericGetWithSort(skips, count, { numberOfViews: -1 });
+      },
+    
+      async getRecentlyVisitedArtworks() {
+        return await genericGetWithSort(skips, count, { lastView: -1 });
+      },
+    
+      async getRecentlyAddedArtworks() {
+        return await genericGetWithSort(skips, count, { postDate: -1 });
+      },
+    
+      async createComment(userId, artworkId, comment) {
+        if (!validators.isNonEmptyString(userId)) throw 'Please provide userId for the comment';
+        if (!validators.isNonEmptyString(artworkId)) throw 'Please provide an artworkId for the comment';
+        if (!validators.isNonEmptyString(comment)) throw 'Please provide the comment string';
+    
+        const user = await users.getUserById(userId);
+        const artwork = await getArtworkById(artworkId);
+    
+        let comment = new models.Comment({
+          userId,
+          username: user.firstName + ' ' + user.lastName,
+          comment,
+        });
+        artwork.comments.push(comment);
+        await artwork.save();
+    
+        return comment;
+      },
+    
+      async deleteComment(artworkId, commentId) {
+        if (!validators.isNonEmptyString(artworkId)) throw 'Please provide artworkId for the comment to delete';
+        if (!validators.isNonEmptyString(commentId)) throw 'Please provide the commentId to delete';
+    
+        const artwork = getArtworkById(artworkId);
+        const comment = artwork.comments.id(commentId);
+        if (comment) {
+          comment.remove();
+        } else {
+          throw "Didn't find a comment with the given ID to delete";
+        }
+      },
+    };
+    
+    async function saveSafely(document) {
+      try {
+        return await document.save();
+      } catch (e) {
+        throw e.message; // Rethrow only the message instead of the whole stack trace
+      }
+    }
+    
+    async function genericGetWithSort(skips = 0, count = 0, sorting) {
+      if (!validators.isPositiveNumber(skips)) throw 'Skips must be positive number';
+      if (!validators.isPositiveNumber(skips)) throw 'Counts must be positive number';
+    
+      return await models.Artwork.find({}).sort(sorting).skip(skips).limit(count);
+    }
     //Functions to add pictures & comments to artwork
     
-    };
     module.exports = exportFunctions;
---
+++ data/validators.js
@@ -0,0 +1,22 @@
+  
+module.exports = {
+    isNonEmptyString(inputString) {
+      return (
+        inputString && typeof inputString === 'string' && inputString.length > 0
+      );
+    },
+    isPositiveNumber(num) {
+      return typeof num === 'number' && num >= 0;
+    },
+    isValidEmail(email) {
+      let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+      return typeof email === 'string' && email.match(mailformat);
+    },
+    isValidPassword(pw) {
+      // to-do
+      return this.isNonEmptyString(pw);
+    },
+    isValidUserId(userId){
+        return typeof userId == 'string' && userId;
+    }
+  };

6fdd08066a3cc1fd38fb767e8d9e280462ce17d2
Ayman Elkfrawy <elkfrawy@gmail.com>
2020-08-13 18:08:07 -0700

    Add navbar with login/register buttons

--- app.js
+++ app.js
@@ -31,7 +31,26 @@ app.use(express.json());
 app.use('/public', static);
 app.use(express.urlencoded({ extended: true }));
 
-app.engine('handlebars', ehb({ defaultLayout: 'main', handlebars: allowInsecurePrototypeAccess(Handlebars) }));
+app.use(function (req, res, next) {
+  res.locals.session = req.session;
+  next();
+});
+
+const handlebarsInst = ehb.create({
+  defaultLayout: 'main',
+  // Specify helpers which are only registered on this instance.
+  helpers: {
+    asJSON: (obj, spacing) => {
+      if (typeof spacing === 'number') return new Handlebars.SafeString(JSON.stringify(obj, null, spacing));
+
+      return new Handlebars.SafeString(JSON.stringify(obj));
+    },
+  },
+  partialsDir: ['views/partials/'],
+  handlebars: allowInsecurePrototypeAccess(Handlebars),
+});
+
+app.engine('handlebars', handlebarsInst.engine);
 app.set('view engine', 'handlebars');
 
 configRoutes(app);
